4198a58053bf9241d75666bcf34646ed46a761fd
yoav31 <yoav31yv@gmail.com>
2025-06-07 01:24:17 +0300

    Update Upload.jsx

--- my-app/src/Upload.jsx
+++ my-app/src/Upload.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import {handle_Download_Transcription_Txt , handle_Download_Transcription_Pdf, handle_Download_Transcription_Word, handle_Download_Summary_Txt, handle_Download_Summary_Pdf,handle_Download_Summary_Word} from './backend/DownFormat'; // importing download functions
-import { handleUpload, transcribe, summarize, transcription,summary } from './backend/api'; // importing upload and processing functions
+import { handleUpload, transcribe, summarize} from './backend/api'; // importing upload and processing functions
 import { Button, Typography, Box } from '@mui/material';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -19,7 +18,6 @@ function Upload() {
   const [language_input, setLanguage_input] = useState('');   // State to store the language of the audio/video
   const [language_transcription, setLanguage_transcription] = useState(''); // State to store the language of the transcription
   const [language_summary, setLanguage_summary] = useState(''); // State to store the language of the summary
-  const [Show_Language_Buttons, Set_Show_Language_Buttons] = useState(false);   // State to control the visibility of download buttons
   const [Show_Language_Transcription_Button, Set_Show_Language_Transcription_Button] = useState(false);   // State to control the visibility of download buttons for transcription
   const [Show_Language_Summary_Button, Set_Show_Language_Summary_Button] = useState(false);   // State to control the visibility of download buttons for summary
   const [Show_Download_Transcription_Buttons, Set_Show_Download_Transcription_Buttons] = useState(false);   // State to control the visibility of download buttons for transcription
@@ -29,6 +27,8 @@ function Upload() {
   const [loadingTranscription, setLoadingTranscription] = useState(false);
   const [Show_Transcribe_Button, Set_Show_Transcribe_Button] = useState(false); // State to control the visibility of the transcription button
   const [Show_Summarize_Button, Set_Show_Summarize_Button] = useState(false); // State to control the visibility of the summary button
+  const [transcription, setTranscription] = useState(''); // State to store the transcription text
+  const [summarization, setSummarization] = useState(''); // State to store the summary text
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
@@ -36,12 +36,6 @@ function Upload() {
     setFile(null);
     document.getElementById("file-upload").value = "";
   };
-  const handleShowLanguageTranscriptionButton = () => {
-    Set_Show_Language_Transcription_Button(true);
-  }
-  const handleShowLanguageSummaryButton = () => {
-    Set_Show_Language_Summary_Button(true);
-  }
 
   const handleAudioChange = (event) => {
     setLanguage_input(event.target.value);
@@ -49,13 +43,30 @@ function Upload() {
 
   const handleLanguageTranscriptionChange = (event) => {
     setLanguage_transcription(event.target.value);
-       
-};
+
+  };
   const handleLanguageSummaryChange = (event) => {
     setLanguage_summary(event.target.value);
-    
+
   };
-  
+
+  const handleTranscribe = async () => {
+    if (!file || !language_transcription) {
+      alert("Missing file or target language.");
+      return;
+    }
+    const transcription = await transcribe(file, language_transcription, setLoadingTranscription, Set_Show_Download_Transcription_Buttons);
+    setTranscription(transcription);
+  }
+  const handleSummarize = async () => {
+    if (!file || !language_summary) {
+      alert("Missing file or target language.");
+      return;
+    }
+    const summarization = await summarize(file, language_summary, setLoadingSummary, Set_Show_Download_Summary_Buttons);
+    setSummarization(summarization);
+  };
+
   const buttonStyle = {
     backgroundColor: '#1032c7',
     color: '#f8f8f8',
@@ -104,7 +115,7 @@ function Upload() {
         id="file-upload"
         name="file-upload"
         inputProps={{ accept: ".mp3, .wav, .flac, .ogg, .m4a, .mp4, .avi, .mov, .mkv, .webm" }}
-        onChange={handleFileChange}/>
+        onChange={handleFileChange} />
 
 
       <Button className="clear-upload" sx={buttonStyle} type="button" onClick={handleClearFile}>Clear File</Button>
@@ -116,52 +127,43 @@ function Upload() {
         <Typography variant='body1'>The file will be sent to the server for processing.</Typography>
         <Typography variant='body1'>Please wait for the processing to complete.</Typography>
 
-        <Button className="upload-button"sx={buttonStyle}type="button" onClick={() => handleUpload(file, language_input, setLoading, Set_Show_Language_Buttons)} disabled={loading}>
-  {loading ? (
-    <Box display="flex" alignItems="center">
-      <CircularProgress size={20} sx={{ marginRight: '8px', color: 'white' }} />
-      <Typography variant="body2" sx={{ color: 'white' }}>
-        Uploading...
-      </Typography>
-    </Box>
-  ) : (
-    "Upload"
-  )}
-</Button>
+        <Button className="upload-button" sx={buttonStyle} type="button" onClick={() => handleUpload(file, language_input, setLoading, Set_Show_Language_Transcription_Button, Set_Show_Language_Summary_Button)} disabled={loading}>
+          {loading ? (
+            <Box display="flex" alignItems="center">
+              <CircularProgress size={20} sx={{ marginRight: '8px', color: 'white' }} />
+              <Typography variant="body2" sx={{ color: 'white' }}>
+                Uploading...
+              </Typography>
+            </Box>
+          ) : ("Upload")}
+        </Button>
       </Box>
 
       <br></br>
 
-      {Show_Language_Buttons && (<>
-        <Box>
-          <Typography variant='p'>Choose:<tab> </tab></Typography>
-          <Button sx={buttonStyle} type='button' onClick={handleShowLanguageTranscriptionButton}>Transcription</Button>
-          <Button sx={buttonStyle} type='button' onClick={handleShowLanguageSummaryButton}>Summary</Button>
-        </Box>
-      </>)}
       <br></br>
       <Box>
-      {Show_Language_Transcription_Button && (<> <TranscriptionLanguageButtons language_transcription={language_transcription} handleLanguageTranscriptionChange={handleLanguageTranscriptionChange} Set_Show_Transcribe_Button={Set_Show_Transcribe_Button} /></>)}
-      <br></br>
-      {Show_Transcribe_Button && 
-      <><Typography variant='p'>Click the button to transcribe the audio/video.</Typography>
-      <Button sx={buttonStyle} type='button' onClick={() => transcribe(file, language_transcription, setLoadingTranscription, Set_Show_Download_Transcription_Buttons)}>Transcribe</Button></>}
-      {loadingTranscription  && <Loading text="transcribing..." />}
+        {Show_Language_Transcription_Button && ( <TranscriptionLanguageButtons language_transcription={language_transcription} handleLanguageTranscriptionChange={handleLanguageTranscriptionChange} Set_Show_Transcribe_Button={Set_Show_Transcribe_Button} />)}
+        <br></br>
+        {Show_Transcribe_Button &&
+          <><Typography variant='p'>Click the button to transcribe the audio/video.</Typography>
+            <Button sx={buttonStyle} type='button' onClick={handleTranscribe}>Transcribe</Button></>}
+        {loadingTranscription && <Loading text="transcribing..." />}
       </Box>
-      <br></br>  
-      
-      {Show_Download_Transcription_Buttons && <TranscriptDownloadButton />}
+      <br></br>
+
+      {Show_Download_Transcription_Buttons && <TranscriptDownloadButton transcription={transcription} />}
       <br></br>
       <Box>
-      {Show_Language_Summary_Button && (<><SummaryLanguageButtons language_summary={language_summary} handleLanguageSummaryChange={handleLanguageSummaryChange} Set_Show_Summarize_Button={Set_Show_Summarize_Button} /></>)}
-      <br></br>
-      {Show_Summarize_Button &&
-      <><Typography variant='p'>Click the button to summarize the transcription.</Typography>
-      <Button sx={buttonStyle} type='button' onClick={() => summarize(file, language_summary, setLoadingSummary, Set_Show_Download_Summary_Buttons)}>summarize</Button></>}
-      {loadingSummary && <Loading text="summarizing..." />}
+        {Show_Language_Summary_Button && (<><SummaryLanguageButtons language_summary={language_summary} handleLanguageSummaryChange={handleLanguageSummaryChange} Set_Show_Summarize_Button={Set_Show_Summarize_Button} /></>)}
+        <br></br>
+        {Show_Summarize_Button &&
+          <><Typography variant='p'>Click the button to summarize the transcription.</Typography>
+            <Button sx={buttonStyle} type='button' onClick={handleSummarize}>summarize</Button></>}
+        {loadingSummary && <Loading text="summarizing..." />}
       </Box>
       <br></br>
-      {Show_Download_Summary_Buttons && <SummaryDownloadButton />}
+      {Show_Download_Summary_Buttons && <SummaryDownloadButton summarization={summarization} />}
     </div>
   );
 }
