0cdf6a1ac9331cf58f9c79e75887c19bcbb425d0
yoav31 <yoav31yv@gmail.com>
2025-06-07 01:27:16 +0300

    Update TranscriptDownloadButton.jsx

--- my-app/src/components/TranscriptDownloadButton.jsx
+++ my-app/src/components/TranscriptDownloadButton.jsx
@@ -5,13 +5,13 @@ import { Box, Typography, Button } from "@mui/material";
 import { handle_Download_Transcription_Txt, handle_Download_Transcription_Pdf, handle_Download_Transcription_Word } from "../backend/DownFormat"; // importing download functions
 import language_transcription from '../Upload';
 
-const TranscriptDownloadButton = () => {
+const TranscriptDownloadButton = ({transcription}) => {
     return (
         <Box style={{ display: "flex", gap: "16px", padding: '8px' }}>
             <Typography variant='h6'>Download Transcript:</Typography>
-            <Button variant="contained" startIcon={<BsFiletypeTxt />} onClick={handle_Download_Transcription_Txt}>.txt </Button>
-            <Button variant="contained" startIcon={< FaRegFilePdf />} onClick={() => handle_Download_Transcription_Pdf(language_transcription)}>.pdf</Button>
-            <Button variant="contained" startIcon={<FaRegFileWord />} onClick={handle_Download_Transcription_Word}>.doc</Button>
+            <Button variant="contained" startIcon={<BsFiletypeTxt />} onClick={() => handle_Download_Transcription_Txt(transcription)}>.txt </Button>
+            <Button variant="contained" startIcon={< FaRegFilePdf />} onClick={() => handle_Download_Transcription_Pdf(language_transcription, transcription)}>.pdf</Button>
+            <Button variant="contained" startIcon={<FaRegFileWord />} onClick={() => handle_Download_Transcription_Word(transcription)}>.doc</Button>
         </Box>
     );
 }
