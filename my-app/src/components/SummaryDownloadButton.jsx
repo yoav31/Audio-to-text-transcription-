9d85e891115d754b4357bace2a0c02fa22a9cf06
yoav31 <yoav31yv@gmail.com>
2025-06-07 01:26:32 +0300

    Update SummaryDownloadButton.jsx

--- my-app/src/components/SummaryDownloadButton.jsx
+++ my-app/src/components/SummaryDownloadButton.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { BsFiletypeTxt } from 'react-icons/bs';
 import { FaRegFilePdf } from "react-icons/fa6";
 import { FaRegFileWord } from "react-icons/fa";
@@ -6,13 +5,13 @@ import { Box, Typography, Button } from "@mui/material";
 import { handle_Download_Summary_Txt, handle_Download_Summary_Pdf, handle_Download_Summary_Word } from "../backend/DownFormat"; // importing download functions
 import language_summary from '../Upload';
 
-const SummeryDownloadButton = () => {
+const SummeryDownloadButton = ({summarization}) => {
     return (
         <Box style={{ display: "flex", gap: "16px", padding: '8px' }}>
-            <Typography variant='h6'>Summary:</Typography>
-            <Button variant="contained" startIcon={<BsFiletypeTxt />} onClick={handle_Download_Summary_Txt}>.txt </Button>
-            <Button variant="contained" startIcon={< FaRegFilePdf />} onClick={() => handle_Download_Summary_Pdf(language_summary)}>.pdf</Button>
-            <Button variant="contained" startIcon={<FaRegFileWord />} onClick={handle_Download_Summary_Word}>.doc</Button>
+            <Typography variant='h6'>Download Summary:</Typography>
+            <Button variant="contained" startIcon={<BsFiletypeTxt />} onClick={() => handle_Download_Summary_Txt(summarization)}>.txt </Button>
+            <Button variant="contained" startIcon={< FaRegFilePdf />} onClick={() => handle_Download_Summary_Pdf(language_summary, summarization)}>.pdf</Button>
+            <Button variant="contained" startIcon={<FaRegFileWord />} onClick={() => handle_Download_Summary_Word(summarization)}>.doc</Button>
         </Box>
     );
 }
